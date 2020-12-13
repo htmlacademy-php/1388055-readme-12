7bc9471ea71b2300f8b0f41aaaf5062831c40a1b
Diana <diana8.84@mail.ru>
2020-12-13 11:21:01 +0300

    if

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $is_auth = rand(0, 1);
 
-$user_name = ''; // укажите здесь ваше имя
+$user_name = 'Диана'; // укажите здесь ваше имя
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -40,8 +40,8 @@ $user_name = ''; // укажите здесь ваше имя
             </div>
         </form>
         <div class="header__nav-wrapper">
-            <!-- здесь должен быть PHP код, который показывает следующий тег по условию -->
-            <nav class="header__nav">
+        <?php if ($is_auth == 1): ?>
+            <nav class="header__nav"> 
                 <ul class="header__my-nav">
                     <li class="header__my-page header__my-page--popular">
                         <a class="header__page-link header__page-link--active" title="Популярный контент">
@@ -110,6 +110,7 @@ $user_name = ''; // укажите здесь ваше имя
                     </li>
                 </ul>
             </nav>
+            <?php endif; ?>
         </div>
     </div>
 </header>
